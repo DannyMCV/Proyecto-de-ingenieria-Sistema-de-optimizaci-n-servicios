bedda69fe11b9ab2af7f69b12c2f7784e7c9c161
Daniel Castelwhite <danielmaucasvega@gmail.com>
2026-02-17 21:14:55 -0500

    Se agrega codigo para arrancar el aplicativo en el run.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
-print("hola mundo");
+# app/__init__.py
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    # Configuración básica
+    app.config["SECRET_KEY"] = "clave-secreta-temporal"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///consumo.db"
+
+    # Registrar blueprints (rutas)
+    from app.routes.consumo import consumo_bp
+    app.register_blueprint(consumo_bp)
+
+    return app
